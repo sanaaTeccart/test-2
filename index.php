02aff770248d73698310f38461643adb001cffda
sanaaTeccart <sanaa.lay@gmail.com>
2023-09-01 16:10:35 -0400

    creation classe personne et affichage sur index

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require_once "Personne.php";
+
+$peter = new Personne();
+$peter -> setName("Peter c'est une personne");
+     
+echo $peter->getName();
+
+
+
+
+
+?>
---
+++ personne.php
@@ -0,0 +1,25 @@
+<?php
+class Personne{
+    public $name;
+    public $address;
+    public $zipcode;
+    public $phone;
+    public $email;
+
+    public function getName(){
+        return $this->name;
+    }
+    
+    public function setName($name){
+         $this->name = $name;
+    }
+
+
+
+
+
+
+
+
+}
+?>
